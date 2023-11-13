781984cfb97016ca11127edb461e549a10530fdb
Marion <marion.haney@gmail.com>
2023-11-13 12:03:03 -0500

    Added new vars

--- load_data.py
+++ load_data.py
@@ -11,8 +11,13 @@ raw_scorecard = pd.read_csv("data/MERGED2018_19_PP.csv")
 scorecard = raw_scorecard.loc[:, ["UNITID", "ACCREDAGENCY", "PREDDEG", "HIGHDEG",
                                   "CONTROL", "REGION", "CCBASIC", "ADM_RATE", 
                                   "CCBASIC", "ADM_RATE", "TUITIONFEE_IN", "TUITIONFEE_OUT",
-                                  "TUITIONFEE_PROG", "TUITFTE", "AVGFACSAL", "CDR2", "CDR3"]]
+                                  "TUITIONFEE_PROG", "TUITFTE", "AVGFACSAL", "CDR2", "CDR3",
+                                  "SAT_AVG", "PCTFLOAN"]]
 
+# Rename UNITID to OPEID to match the IPEDS data
+scorecard.rename(columns={"UNITID": "OPEID"})
+
+# Check the output
 scorecard.head()
 
 # Keep only the columns we want from the IPEDS data
@@ -20,3 +25,6 @@ ipeds = raw_ipeds.loc[:, ["INSTNM", "ADDR", "ZIP", "FIPS", "CITY", "STABBR",
                           "OPEID", "CBSA", "CSA", "LONGITUD", "LATITUDE"]]
 
 ipeds.head()
+
+# Join the datasets together
+#data = 
